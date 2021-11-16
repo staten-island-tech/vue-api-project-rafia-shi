18ca31f188e9ee6dfe139c10df90ea5047eb81d7
SHI <VSHI@C56.LOCAL>
2021-11-16 12:27:02 -0500

    ok
    ok

--- rafi-shi/src/App.vue
+++ rafi-shi/src/App.vue
@@ -10,12 +10,14 @@
 export default{
   data(){
     return{
+  
+      datab: [],
+    };
+  },
 
-    }
-  }
-
-}
-
+  methods: {
+    fetch('https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15')
+    .then(data =>)
 </script>
 
 <style>
