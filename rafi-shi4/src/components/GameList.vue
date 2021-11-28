2c91e069fabdccee5afdefff16c3dfe2b3dbeb34
Rafia7 <raf.arnob8@gmail.com>
2021-11-28 16:08:58 -0500

    Vinny's time to shine

---
+++ rafi-shi4/src/components/GameList.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <h1>API Project</h1>
+    <ul class="game-table">
+      <li
+        class="game-list-item"
+        v-for="game in games" :key="game.title"
+      >
+        {{game.title}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GameList",
+  data() {
+    return { games: [] };
+  },
+  created: function() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: async function() {
+      try {
+        const result = await fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15`
+          
+        );
+        const apiData = await result.json();
+        this.games = apiData;
+        console.log(apiData);
+      } catch (error) {
+        alert(error);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+ul{
+  list-style-type: none;
+}
+h1 {
+  font-size: 2.5rem;
+  font-family: 'Courier New', Courier, monospace;
+  text-align: center;
+  background-color: white;
+  color: orange;
+  
+  padding: 1.3rem;
+ 
+}
+li {
+  color: black;
+  
+}
+body {
+  background-color: orange;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 2rem;
+  font-weight: bold;
+  justify-content: center;
+  font-family: 'Courier New', Courier, monospace;
+}
+.game-table{
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  justify-content: space-evenly;
+
+}
+.game-list-item{
+  color: white;
+  font-family: 'Courier New', Courier, monospace;
+  border-radius: 1.5rem;
+  font-size: 1rem;
+  background-color: tomato;
+}
+/*.test{
+  text-align: center;
+  background-color: white;
+  color: orange;
+  
+  padding: 1.3rem;
+ 
+}
+.logo{
+  background-image: url("./assets/Crunchyroll.png");
+}
+.btn{
+  border-radius: 1.5rem;
+  background-color: aquamarine;
+  text-align: center;
+  align-items: center;
+}*/
+</style>
--- rafi-shi4/src/views/Home.vue
+++ rafi-shi4/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <GameList />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import GameList from "../components/GameList";
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    GameList,
   },
 };
 </script>
+© 2021 GitHub, Inc.
+© 2021 GitHub, Inc.
