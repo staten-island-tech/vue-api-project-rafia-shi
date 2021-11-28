5ff355f20e062c753999294324447769436cc562
Rafia7 <raf.arnob8@gmail.com>
2021-11-28 14:41:06 -0500

    please

---
+++ rafi-shi3/src/components/GameList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>TEST</h1>
+    <ul class="game-table">
+      <li
+        class="game-list-item"
+        v-for="game in games" :key="game.title"
+      >
+        {{game.title}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GameList",
+  data() {
+    return { games: [] };
+  },
+  created: function() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: async function() {
+      try {
+        const result = await fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15`
+          
+        );
+        const apiData = await result.json();
+        this.games = apiData;
+        console.log(apiData);
+      } catch (error) {
+        alert(error);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+h1 {
+  font-size: 5rem;
+  color: black;
+}
+li {
+  color: black;
+}
+</style>
---
+++ rafi-shi3/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <GameList />
+  </div>
+</template>
+
+<script>
+import GameList from "../components/GameList";
+export default {
+  name: "Home",
+  components: {
+    GameList,
+  },
+};
+</script>
+© 2021 GitHub, Inc.
